7945f39fac675a8511690f5965b4df9a1e7b64cb
samarth29jc <samarth29jc@gmail.com>
2024-07-11 10:40:09 +0530

    first commit

---
+++ 1st_reactapp/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <h1>Samarth this side</h1>
+      <h1>Address: Indore</h1>
+      <h1>Final year grad</h1>
+      <h1>E.no: btcse09958</h1>
+    </div>
+  );
+}
+
+export default App;
